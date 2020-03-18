2079682596dba85dbdcdd2ee9300a106eec42981
surhidamatya <surhid.amatya@gmail.com>
2020-03-18 17:41:51 +0545

    integrateTMDB: remove unused methods

--- src/utils/utilityGenerator.js
+++ src/utils/utilityGenerator.js
@@ -1,18 +1,6 @@
 import { getTmdbIds } from "../services/services";
 import * as constant from "../utils/constants";
 
-export function concatenate(str, params) {
-  let formattedString = str;
-
-  for (const [key, value] of Object.entries(params)) {
-    const val = value || '';
-
-    formattedString = formattedString.replace(new RegExp(':' + key, 'gi'), val.toString());
-  }
-
-  return formattedString;
-}
-
 export async function findImdbId(id, media_type) {
   const externalId = await getTmdbIds(id, media_type);
   const imdbId = externalId.data.imdb_id;
