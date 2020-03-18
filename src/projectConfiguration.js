656e7bd8076635cb43cbc16fb938d34a2073d7bd
surhidamatya <surhid.amatya@gmail.com>
2020-03-18 17:35:36 +0545

    integrateTMDB: define api key in constant file

--- src/projectConfiguration.js
+++ src/projectConfiguration.js
@@ -1,6 +1,6 @@
 const trendingUrl = '/trending';
 const posterSize = '/w800';
-const genres = '/genre/movie/list?api_key=9626168d181bfa64fe2b0fa44150ce6b';
+const genres = '/genre/movie/list';
 
 const REACT_APP_API_URL='https://api.themoviedb.org/3';
 const REACT_APP_IMG_URL= 'https://image.tmdb.org/t/p/original';
--- src/services/services.js
+++ src/services/services.js
@@ -2,21 +2,21 @@ import http from '../utils/http';
 import { projectConfiguration } from '../projectConfiguration';
 
 export async function getTrendingItems(mediaType, timeWindow) {
-	const url = `${projectConfiguration.trendingUrl}/${mediaType}/${timeWindow}?api_key=9626168d181bfa64fe2b0fa44150ce6b`;
+	const url = `${projectConfiguration.trendingUrl}/${mediaType}/${timeWindow}?api_key=${projectConfiguration.token}`;
 	const response = await http.get(url);
 
 	return response;
 }
 
 export async function getGenres() {
-  const url = projectConfiguration.genreUrl;
+  const url = `${projectConfiguration.genreUrl}?api_key=${projectConfiguration.token}`;
   const response = await http.get(url);
 
   return response;
 }
 
 export async function getTmdbIds(tmdbId, mediaType) {
-  const url = `${projectConfiguration.apiURL}/${mediaType}/${tmdbId}/external_ids?api_key=9626168d181bfa64fe2b0fa44150ce6b`;
+  const url = `${projectConfiguration.apiURL}/${mediaType}/${tmdbId}/external_ids?api_key=${projectConfiguration.token}`;
   const response = await http.get(url);
 
   return response;
